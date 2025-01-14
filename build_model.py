7338e7bac54d96b5ac5783b7582676cf00389ce6
libertymxc <liberty1213@163.com>
2025-01-14 16:39:05 +0800

    first commit

--- build_model.py
+++ build_model.py
@@ -10,7 +10,7 @@ def build_model(args, device, model_name, num_features, num_nodes):
     if model_name == 'gcn':
         model = BrainNN(args,
                       GCN(num_features, args, num_nodes),
-                      MLP(2 * num_nodes, args.hidden_dim, args.n_MLP_layers, torch.nn.ReLU, n_classes=5),
+                      MLP(2 * num_nodes, args.hidden_dim, args.n_MLP_layers, torch.nn.ReLU, n_classes=args.num_classes),
                       ).to(device)
     else:
         raise ValueError(f"ERROR: Model variant \"{args.variant}\" not found!")
